426cb299be338f158bcd5e1fab5d8c378483d5b1
FarhanMawaludin <farhanmawaludin7@gmail.com>
2025-05-09 14:06:17 +0700

    sidebar

--- resources/views/lowongan/index.blade.php
+++ resources/views/lowongan/index.blade.php
@@ -55,7 +55,10 @@
             </div>
         </form>
 
-        <a href="{{ route('lowongan.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">Tambah</a>
+        <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button" onclick="location.href='{{ url('lowongan/create') }}'">Tambah Lowongan
+        </button>
     </div>
 
     <div class="overflow-x-auto relative rounded-lg border border-gray-200">
